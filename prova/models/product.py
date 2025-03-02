d1ca09fb7166087109bcc47a0f3f3db83444a1cf
giu <giusygiallara93@gmail.com>
2025-03-02 18:59:45 +0100

    [17.0]add new module

--- prova/models/product.py
+++
@@ -1,8 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    prodotti_da_approvvigionare_ids = fields.One2many('prodotti.approvvigionare', 'product_id',string='Prodotti')
-
